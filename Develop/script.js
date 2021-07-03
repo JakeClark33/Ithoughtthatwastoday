8e891967bfd4492155b0f240bda6aa1ef15517da
JakeClark33 <84428608+JakeClark33@users.noreply.github.com>
2021-07-02 23:39:13 -0500

    time

---
+++ Develop/script.js
@@ -0,0 +1,73 @@
+//ability to add events to planner
+
+
+
+//Whe I open the planner, the current day is displayed at the top
+
+function renderTime() {
+    var mydate= new Date();
+    var year = mydate.getFullYear();
+        if(year < 1000) {
+            year =-1900
+        } 
+       var day = mydate.getDay();
+       var month = mydate.getMonth(); 
+       var daym = mydate.getDate();
+       var dayArray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+       var monthArray = new Array("January","February","March","April","May","June","July","August","September","October","November","December",);
+    
+//Date End
+
+//Time
+        var currentTime = new Date();
+        var h = currentTime.getHours();
+        var m = currentTime.getMinutes();
+        var s = currentTime.getSeconds();
+            if(h == 24){
+                h = 0;
+            } else if (h > 12){
+                h = h - 0;
+            }
+                if (h < 10) {
+                    h = "0" + h;
+                }
+                if ( m < 10) {
+                    m = "0" + m;
+                }
+                if(s < 10) {
+                    s = "0" + s;
+                }
+
+                var myClock = document.getElementById("currentDay");
+                myClock.textContent = "" + dayArray[day]+ " " +monthArray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
+                myClock,innerText = "" + dayArray[day]+ " " +monthArray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
+            
+                setTimeout("renderTime()", 1000);
+            }
+            renderTime();
+
+
+//When I scroll I am give time blocks for a standard businedd day
+
+
+
+
+//Each time block is color coded for past present or future
+
+
+
+
+//When I click into a time block I can enter an event
+
+
+
+
+// When i click save for that time block the event is saved in local storage
+
+
+
+
+//When i refresh the page, the save persists
+
+
+
